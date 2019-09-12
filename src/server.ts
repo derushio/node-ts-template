502cfbaca36b8ef94691d0195bfcbdb7a528b10c
derushio <narumi18wa@gmail.com>
2019-09-12 17:32:49 +0900

    <master>[modified] add body parser

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const port = 8080;
 const execPath = path.dirname(process.argv[1]);
@@ -7,7 +8,11 @@ const publicPath = path.resolve(execPath, '../public');
 
 export async function initServer() {
     const server = express();
+    server.use(bodyParser.urlencoded({ extended: true }));
+    server.use(bodyParser.json());
+
     server.use(express.static(publicPath));
+
     server.listen(port);
     console.log('ENV_LOG:', `http://localhost:${port} --> ${publicPath}`);
 }
