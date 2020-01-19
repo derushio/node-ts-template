88628092409cfaff0a26abdaa3bda0d12cc1754c
derushio <derushio@gmail.com>
2020-01-20 03:46:53 +0900

    <master>[new file] update

--- src/utils/RandomUtil.ts
+++ src/utils/RandomUtil.ts
@@ -1,9 +1,15 @@
+/**
+ * ランダムユーティルクラス
+ */
 export default class RandomUtil {
+    /**
+     * startからend(含む)までの整数乱数を生成する, startのみの場合は指定数の乱数を生成する
+     */
     public static rand(start: number, end?: number) {
         if (end == null) {
-            return Math.random() * start;
+            return Math.floor(Math.random() * start);
         }
-        return Math.floor(Math.random() * (end - start) + start);
+        return Math.floor(Math.random() * (end + 1 - start) + start);
     }
 
     protected constructor() {}
